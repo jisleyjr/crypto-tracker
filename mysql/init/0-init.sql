4db9cf44dc890113314b2a8e9b9da9ce62adc076
John Isley <jisley@bushelpowered.com>
2022-12-28 10:10:37 -0600

    Consolidated the table creations.

--- mysql/init/0-init.sql
+++ mysql/init/0-init.sql
@@ -25,5 +25,34 @@ CREATE TABLE `transactions` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*
- User_Id,Time,Category,Operation,Order_Id,Transaction_Id,Primary_Asset,Realized_Amount_For_Primary_Asset,Realized_Amount_For_Primary_Asset_In_USD_Value,Base_Asset,Realized_Amount_For_Base_Asset,Realized_Amount_For_Base_Asset_In_USD_Value,Quote_Asset,Realized_Amount_For_Quote_Asset,Realized_Amount_For_Quote_Asset_In_USD_Value,Fee_Asset,Realized_Amount_For_Fee_Asset,Realized_Amount_For_Fee_Asset_In_USD_Value,Payment_Method,Withdrawal_Method,Additional_Note*/
+CREATE TABLE `positions` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Order_Id` varchar(50) DEFAULT NULL,
+  `Order_Date` timestamp,
+  `Coin` varchar(50) DEFAULT NULL,
+  `Original_Qty` DECIMAL(65,9) DEFAULT NULL,
+  `Remaining_Qty` DECIMAL(65,9) DEFAULT NULL,
+  `Price` DECIMAL(65,6) DEFAULT NULL,
+  `Total_Cost` DECIMAL(65,6) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `sales` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Order_Id` varchar(50) DEFAULT NULL,
+  `Order_Date` timestamp,
+  `Coin` varchar(50) DEFAULT NULL,
+  `Qty` DECIMAL(65,9) DEFAULT NULL,
+  `Price` DECIMAL(65,6) DEFAULT NULL,
+  `Total_Proceeds` DECIMAL(65,6) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `position_sales` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Position_Id` int(11) NOT NULL,
+  `Sale_Id` int(11) NOT NULL,
+  `Qty` DECIMAL(65,9) NOT NULL,
+   PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
--- mysql/init/create_position_sales.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `position_sales` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Position_Id` int(11) NOT NULL,
-  `Sale_Id` int(11) NOT NULL,
-  `Qty` DECIMAL(65,9) NOT NULL,
-   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
