88f3058423200bfe4b0f9b9d95a96cffa28d0db2
John Isley <jisley@bushelpowered.com>
2023-12-15 14:58:25 -0600

    Created the file to import a csv into position_sales

--- import_binance_csv/src/migration.py
+++ import_binance_csv/src/migration.py
@@ -24,7 +24,43 @@ try:
                                  database='crypto-tracker')
 
     with open(filename, newline='\n') as csvfile:
+        reader = csv.DictReader(csvfile)
 
+        data_transactions = []
+
+        add_transaction = ("INSERT INTO position_sales (Position_Id, Sale_Id, Qty) "
+                           "VALUES (%(position_id)s, %(sale_id)s, %(qty)s)")
+        cursor = cnx.cursor(buffered=True)
+
+        for row in reader:
+            print('Check if position_sales exists')
+
+            query = ("SELECT Id FROM position_sales "
+                "WHERE Position_Id = %s AND Sale_Id = %s")
+
+            cursor.execute(query, (row['Position_Id'], row['Sale_Id']))
+
+            if cursor.rowcount > 0:
+                print('Record already exists')
+            else:
+                data_transaction = {
+                    'position_id': row['Position_Id'],
+                    'sale_id': row['Sale_Id'],
+                    'qty': row['Qty']
+                }
+
+                print('Adding to array')
+                data_transactions.append(data_transaction)
+        
+        print('Executing bulk insert sql')
+        
+        cursor.executemany(add_transaction, data_transactions)
+
+        # Make sure data is committed to the database
+        cnx.commit()
+        cursor.close()
+
+        print('Done')
 
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
