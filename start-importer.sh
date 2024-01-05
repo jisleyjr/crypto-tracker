73905947c30f404a616149394d333a2f43986efb
John Isley <jisley@bushelpowered.com>
2024-01-05 15:59:04 -0600

    Added migration folder

--- start-importer.sh
+++ start-importer.sh
@@ -3,5 +3,6 @@ echo 'Launching Importer'
 
 FILEDIR="`pwd`/import_binance_csv/files"
 SRCDIR="`pwd`/import_binance_csv/src"
+MIGDIR="`pwd`/import_binance_csv/migration"
 
-docker run --name import_binance_csv --network crypto-tracker_default -v $FILEDIR:/media -v $SRCDIR:/app -it --rm import_binance_csv:latest
+docker run --name import_binance_csv --network crypto-tracker_default -v $FILEDIR:/media -v $SRCDIR:/app -v $MIGDIR:/home -it --rm --entrypoint /bin/bash import_binance_csv:latest
