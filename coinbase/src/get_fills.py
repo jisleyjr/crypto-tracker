081f8aeb65e9b7afbd4dc857bcdc391bc605bf0f
John Isley <jisley@bushelpowered.com>
2025-04-12 09:59:13 -0500

    Created files folder

--- coinbase/src/get_fills.py
+++ coinbase/src/get_fills.py
@@ -17,7 +17,7 @@ fills = client.get_fills(
 
 order_dict = dumps(fills.to_dict(), indent=2)
 
-output_filename = f"fills_{year}.json"
+output_filename = f"files/fills_{year}.json"
 
 with open(output_filename, 'w', encoding='utf-8') as f:
     json.dump(fills.to_dict(), f, ensure_ascii=False, indent=4)
