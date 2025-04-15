9d27dd91350cd6ef1ffb793b4e59028341101d39
John Isley <jisley@bushelpowered.com>
2025-04-15 08:20:53 -0500

    coinbase file for trying things out.

--- coinbase/src/cb.py
+++ coinbase/src/cb.py
@@ -3,18 +3,31 @@ from json import dumps
 
 client = RESTClient()
 
-#accounts = client.get_accounts()
-#print(dumps(accounts, indent=2))
+# Get orders
+orders = client.list_orders()
+order_dict = orders.to_dict('records')
+print("Orders:")
+# Print the first order
 
-market_trades = client.get("/api/v3/brokerage/orders/historical/batch", 
-                           params={
-                               "limit": 25, 
-                               "product_id": "BTC-USD",
-                               "order_status": "FILLED",
-                               "order_side": "BUY"
-                               }
-                            )
-print(dumps(market_trades, indent=2))
+print(dumps(order_dict, indent=2))
+#print(dumps(, indent=2))
+
+# Create a variable to hold the year
+year = 2024
+
+print(f"Get transactinons for year {year}")
+
+#market_trades = client.get("/api/v3/brokerage/orders/historical/batch", 
+#                           params={
+#                               "limit": 25, 
+#                               "product_id": "BTC-USD",
+#                               "order_status": "FILLED",
+#                               "order_side": "BUY"
+#                               }
+#                            )
+
+#market_trades = client.get_market_trades(product_id="BTC-USD", limit=5)
+#print(dumps(market_trades.to_dict(), indent=2))
 
 
 #portfolio = client.post("/api/v3/brokerage/portfolios", data={"name": "TestPortfolio"})
