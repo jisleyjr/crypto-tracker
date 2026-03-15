4dabf352cbcc38833c15dc807cc26412c43b4bb6
John Isley <john.isley@gmail.com>
2026-03-14 22:40:51 -0500

    Added a year drop down to the sales list.

---
+++ app/src/components/SalesList.js
@@ -0,0 +1,65 @@
+import './App.css';
+import DataTable from 'react-data-table-component';
+import { useQuery } from '@tanstack/react-query';
+
+const columns = [
+	{
+		name: 'Coin',
+		selector: row => row.coin,
+	},
+    {
+		name: 'Buy Date',
+		selector: row => row.buy_date,
+        format: row => new Date(row.buy_date).toLocaleDateString(),
+	},
+	{
+		name: 'Sales Date',
+		selector: row => row.sales_date,
+        format: row => new Date(row.sales_date).toLocaleDateString(),
+	},
+    {
+        name: 'Proceeds',
+        selector: row => row.actual_proceeds,
+    },
+    {
+        name: 'Cost',
+        selector: row => row.actual_cost,
+    },
+    {
+        name: 'Gains / Losses',
+        selector: row => row.gains_losses,
+    },
+    {
+        name: 'Source',
+        selector: row => row.source,
+    }
+];
+
+function SalesList({selectedYear}) {
+    const { data: sales, isLoading, error } = useQuery({
+        queryFn: () =>
+          fetch(`http://localhost:5000/sales/${selectedYear}`).then(
+            (res) => res.json()
+          ),
+        queryKey: [`sales_${selectedYear}`],
+    });
+
+    // Show a loading message while data is fetching
+    if (isLoading) {
+        return <h2>Loading...</h2>;
+    }
+    
+    // to handle error
+    if (error) {
+        return <div className="error">Error: error fetching</div>
+    }
+
+    return (
+        <DataTable
+            columns={columns}
+            data={sales}
+        />
+    )
+}
+
+export default SalesList;
