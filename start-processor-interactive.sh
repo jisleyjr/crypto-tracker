1f1c8a36ae9e60b1ac91d3e26723dcc4529eba3c
John Isley <jisley@bushelpowered.com>
2026-04-14 21:12:14 -0500

    Removing ignores file

--- start-processor-interactive.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 'Launching Processor'
-
-SRCDIR="`pwd`/processor/src"
-
-docker run --name crypto-tracker-processor --network crypto-tracker_default -v $SRCDIR:/app -it --rm --entrypoint=/bin/bash crypto-tracker-processor:latest
