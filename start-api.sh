707c1a6d6765f06ebe32862b2bcd26eb28fd8340
John Isley <john.isley@gmail.com>
2026-04-17 22:45:45 -0500

    Update start to pull in env

--- start-api.sh
+++ start-api.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo 'Launching API'
 
-SRCDIR="`pwd`/api/src"
+SRCDIR="$(pwd)/api/src"
 
-docker run --network crypto-tracker_default -p 5000:5000 crypto-tracker-api:latest
+docker run --network crypto-tracker_default \
+  -p 5000:5000 \
+  --env-file .env \
+  crypto-tracker-api:latest
